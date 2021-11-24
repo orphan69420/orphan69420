c2e4a216c273f8897b99871111d8805b8e6825ef
orphan69420 <95000895+orphan69420@users.noreply.github.com>
2021-11-24 17:25:04 -0600

    Add files via upload

---
+++ Module1.vb
@@ -0,0 +1,21 @@
+﻿Module Module1
+    Sub Main()
+
+        Dim productid As Integer
+
+        Console.WriteLine("Please enter your Product ID")
+        productid = Console.ReadLine().ToString()
+
+        If productid = 69420 Then
+
+            Console.WriteLine("Welcome to the AmogOS Operating System!")
+            Console.ReadLine()
+        Else
+            Console.WriteLine("Invalid Product ID")
+            Console.ReadLine()
+
+        End If
+
+
+    End Sub
+End Module
